e14dab4bd8b2aa502d932170b479c9518c70edc8
JustB3nny <60150127+JustB3nny@users.noreply.github.com>
2021-03-24 11:37:50 +0000

    Update Form1.Designer.cs

--- FSImageEditor/Form1.Designer.cs
+++ FSImageEditor/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace FSImageEditor
+namespace FSImageEditor
 {
     partial class Form1
     {
@@ -31,16 +31,16 @@
         	this.panel1 = new System.Windows.Forms.Panel();
         	this.panel7 = new System.Windows.Forms.Panel();
         	this.txt_ShapeSize = new System.Windows.Forms.TextBox();
-        	this.label4 = new System.Windows.Forms.Label();
+        	this.lab_ShapeSize = new System.Windows.Forms.Label();
         	this.btn_Circle = new System.Windows.Forms.Button();
         	this.btn_Rectangle = new System.Windows.Forms.Button();
         	this.btn_Square = new System.Windows.Forms.Button();
         	this.panel8 = new System.Windows.Forms.Panel();
-        	this.label3 = new System.Windows.Forms.Label();
+        	this.lab_DrawShapes = new System.Windows.Forms.Label();
         	this.panel2 = new System.Windows.Forms.Panel();
-        	this._buttErase = new System.Windows.Forms.Button();
+        	this.btn_Erase = new System.Windows.Forms.Button();
         	this.panel6 = new System.Windows.Forms.Panel();
-        	this.label2 = new System.Windows.Forms.Label();
+        	this.lab_PenTool = new System.Windows.Forms.Label();
         	this.btn_PenColor = new System.Windows.Forms.Button();
         	this.cmb_PenSize = new System.Windows.Forms.ComboBox();
         	this.menuStrip1 = new System.Windows.Forms.MenuStrip();
@@ -75,7 +75,7 @@
         	this.panel7.BackColor = System.Drawing.Color.Gray;
         	this.panel7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
         	this.panel7.Controls.Add(this.txt_ShapeSize);
-        	this.panel7.Controls.Add(this.label4);
+        	this.panel7.Controls.Add(this.lab_ShapeSize);
         	this.panel7.Controls.Add(this.btn_Circle);
         	this.panel7.Controls.Add(this.btn_Rectangle);
         	this.panel7.Controls.Add(this.btn_Square);
@@ -94,15 +94,15 @@
         	this.txt_ShapeSize.TabIndex = 9;
         	this.txt_ShapeSize.Text = "10";
         	// 
-        	// label4
+        	// lab_ShapeSize
         	// 
-        	this.label4.AutoSize = true;
-        	this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-        	this.label4.Location = new System.Drawing.Point(6, 137);
-        	this.label4.Name = "label4";
-        	this.label4.Size = new System.Drawing.Size(71, 13);
-        	this.label4.TabIndex = 8;
-        	this.label4.Text = "Shape Size";
+        	this.lab_ShapeSize.AutoSize = true;
+        	this.lab_ShapeSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        	this.lab_ShapeSize.Location = new System.Drawing.Point(6, 137);
+        	this.lab_ShapeSize.Name = "lab_ShapeSize";
+        	this.lab_ShapeSize.Size = new System.Drawing.Size(71, 13);
+        	this.lab_ShapeSize.TabIndex = 8;
+        	this.lab_ShapeSize.Text = "Shape Size";
         	// 
         	// btn_Circle
         	// 
@@ -143,28 +143,28 @@
         	// panel8
         	// 
         	this.panel8.BackColor = System.Drawing.Color.DarkGray;
-        	this.panel8.Controls.Add(this.label3);
+        	this.panel8.Controls.Add(this.lab_DrawShapes);
         	this.panel8.Location = new System.Drawing.Point(0, 0);
         	this.panel8.Name = "panel8";
         	this.panel8.Size = new System.Drawing.Size(130, 27);
         	this.panel8.TabIndex = 1;
         	// 
-        	// label3
+        	// lab_DrawShapes
         	// 
-        	this.label3.AutoSize = true;
-        	this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-        	this.label3.ForeColor = System.Drawing.Color.Black;
-        	this.label3.Location = new System.Drawing.Point(15, 5);
-        	this.label3.Name = "label3";
-        	this.label3.Size = new System.Drawing.Size(100, 16);
-        	this.label3.TabIndex = 1;
-        	this.label3.Text = "Draw Shapes";
+        	this.lab_DrawShapes.AutoSize = true;
+        	this.lab_DrawShapes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        	this.lab_DrawShapes.ForeColor = System.Drawing.Color.Black;
+        	this.lab_DrawShapes.Location = new System.Drawing.Point(15, 5);
+        	this.lab_DrawShapes.Name = "lab_DrawShapes";
+        	this.lab_DrawShapes.Size = new System.Drawing.Size(100, 16);
+        	this.lab_DrawShapes.TabIndex = 1;
+        	this.lab_DrawShapes.Text = "Draw Shapes";
         	// 
         	// panel2
         	// 
         	this.panel2.BackColor = System.Drawing.Color.Gray;
         	this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-        	this.panel2.Controls.Add(this._buttErase);
+        	this.panel2.Controls.Add(this.btn_Erase);
         	this.panel2.Controls.Add(this.panel6);
         	this.panel2.Controls.Add(this.btn_PenColor);
         	this.panel2.Controls.Add(this.cmb_PenSize);
@@ -173,35 +173,35 @@
         	this.panel2.Size = new System.Drawing.Size(130, 133);
         	this.panel2.TabIndex = 1;
         	// 
-        	// _buttErase
+        	// btn_Erase
         	// 
-        	this._buttErase.Location = new System.Drawing.Point(4, 99);
-        	this._buttErase.Name = "_buttErase";
-        	this._buttErase.Size = new System.Drawing.Size(122, 28);
-        	this._buttErase.TabIndex = 4;
-        	this._buttErase.Text = "Erase";
-        	this._buttErase.UseVisualStyleBackColor = true;
-        	this._buttErase.Click += new System.EventHandler(this._buttEraseClick);
+        	this.btn_Erase.Location = new System.Drawing.Point(4, 99);
+        	this.btn_Erase.Name = "btn_Erase";
+        	this.btn_Erase.Size = new System.Drawing.Size(122, 28);
+        	this.btn_Erase.TabIndex = 4;
+        	this.btn_Erase.Text = "Erase";
+        	this.btn_Erase.UseVisualStyleBackColor = true;
+        	this.btn_Erase.Click += new System.EventHandler(this._buttEraseClick);
         	// 
         	// panel6
         	// 
         	this.panel6.BackColor = System.Drawing.Color.DarkGray;
-        	this.panel6.Controls.Add(this.label2);
+        	this.panel6.Controls.Add(this.lab_PenTool);
         	this.panel6.Location = new System.Drawing.Point(0, 0);
         	this.panel6.Name = "panel6";
         	this.panel6.Size = new System.Drawing.Size(130, 27);
         	this.panel6.TabIndex = 2;
         	// 
-        	// label2
+        	// lab_PenTool
         	// 
-        	this.label2.AutoSize = true;
-        	this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-        	this.label2.ForeColor = System.Drawing.Color.Black;
-        	this.label2.Location = new System.Drawing.Point(27, 5);
-        	this.label2.Name = "label2";
-        	this.label2.Size = new System.Drawing.Size(71, 16);
-        	this.label2.TabIndex = 1;
-        	this.label2.Text = "Pen Tool";
+        	this.lab_PenTool.AutoSize = true;
+        	this.lab_PenTool.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        	this.lab_PenTool.ForeColor = System.Drawing.Color.Black;
+        	this.lab_PenTool.Location = new System.Drawing.Point(27, 5);
+        	this.lab_PenTool.Name = "lab_PenTool";
+        	this.lab_PenTool.Size = new System.Drawing.Size(71, 16);
+        	this.lab_PenTool.TabIndex = 1;
+        	this.lab_PenTool.Text = "Pen Tool";
         	// 
         	// btn_PenColor
         	// 
@@ -211,7 +211,7 @@
         	this.btn_PenColor.Size = new System.Drawing.Size(122, 28);
         	this.btn_PenColor.TabIndex = 2;
         	this.btn_PenColor.UseVisualStyleBackColor = false;
-        	this.btn_PenColor.Click += new System.EventHandler(this.Button1_Click);
+        	this.btn_PenColor.Click += new System.EventHandler(this.Btn_PenColor_Click);
         	// 
         	// cmb_PenSize
         	// 
@@ -255,7 +255,7 @@
 			this.blankCanvasToolStripMenuItem,
 			this.loadImageToolStripMenuItem});
         	this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-        	this.newToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+        	this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
         	this.newToolStripMenuItem.Text = "New";
         	// 
         	// blankCanvasToolStripMenuItem
@@ -275,14 +275,14 @@
         	// saveToolStripMenuItem
         	// 
         	this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-        	this.saveToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+        	this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
         	this.saveToolStripMenuItem.Text = "Save";
         	this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
         	// 
         	// exitToolStripMenuItem
         	// 
         	this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-        	this.exitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+        	this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
         	this.exitToolStripMenuItem.Text = "Exit";
         	this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
         	// 
@@ -293,9 +293,9 @@
         	this.pictureBox1.Size = new System.Drawing.Size(602, 433);
         	this.pictureBox1.TabIndex = 2;
         	this.pictureBox1.TabStop = false;
-        	this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Pnl_Draw_MouseDown);
-        	this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Pnl_Draw_MouseMove);
-        	this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Pnl_Draw_MouseUp);
+        	this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Draw_MouseDown);
+        	this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Draw_MouseMove);
+        	this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Draw_MouseUp);
         	// 
         	// Form1
         	// 
@@ -337,21 +337,20 @@
         private System.Windows.Forms.Button btn_PenColor;
         private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
         private System.Windows.Forms.Panel panel6;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lab_PenTool;
         private System.Windows.Forms.Panel panel7;
         private System.Windows.Forms.TextBox txt_ShapeSize;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lab_ShapeSize;
         private System.Windows.Forms.Button btn_Circle;
         private System.Windows.Forms.Button btn_Rectangle;
         private System.Windows.Forms.Button btn_Square;
         private System.Windows.Forms.Panel panel8;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lab_DrawShapes;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem blankCanvasToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button _buttErase;
+        private System.Windows.Forms.Button btn_Erase;
     }
 }
-
